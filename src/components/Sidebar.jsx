f5175c4f8adf368aae476ade8b5d7f20b9a6ee85
HenriqueNeubert <neubert.study@gmail.com>
2022-10-25 19:28:49 -0300

    feat: created social-networks

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,14 @@
 import '../styles/components/sidebar.sass'
 import React from 'react'
 import Avatar from '../img/eu.jpeg'
+import SocialNetwork from './SocialNetwork'
 
 const Sidebar = () => {
   return (
     <aside id='sidebar'>
       <img src={ Avatar } alt="Henrique Neubert" />
       <p className='title'>Desenvolvedor</p>
-      <p>Redes sociais</p>
+      <SocialNetwork />
       <p>Informações de contato</p>
       <a href="" className='btn'>
         Download Curriculo
